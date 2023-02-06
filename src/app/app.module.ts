7a8d0ee5d10ed136419c7e65e6199c805947fe28
Estêvão <estevaoscalera@gmail.com>
2023-02-06 10:39:12 -0300

    dark theme app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/pages/components/home/home.component';
+import { SobreComponent } from './core/pages/components/sobre/sobre.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
